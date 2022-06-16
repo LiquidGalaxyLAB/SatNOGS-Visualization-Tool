24fcdcc53827d96f94248c7cf8602dcfee7f34df
Michell Algarra <michell.algarra@gmail.com>
2022-06-15 22:03:05 -0300

    feat(settings.dart): implemented the connection form

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,4 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:satnogs_visualization_tool/utils/colors.dart';
+import 'package:satnogs_visualization_tool/widgets/button.dart';
+import 'package:satnogs_visualization_tool/widgets/input.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({Key? key}) : super(key: key);
@@ -8,6 +13,40 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPageState extends State<SettingsPage> {
+  bool _connected = false;
+  bool _loading = false;
+
+  String _ip = '';
+  String _port = '';
+
+  final _ipController = TextEditingController();
+  final _portController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _initNetworkState();
+  }
+
+  void _initNetworkState() async {
+    try {
+      final ips = await NetworkInterface.list(type: InternetAddressType.IPv4);
+
+      if (ips.isEmpty || ips.first.addresses.isEmpty) {
+        return;
+      }
+
+      setState(() {
+        _ip = ips.first.addresses.first.address;
+        _ipController.text = _ip;
+      });
+    } on Exception {
+      setState(() {
+        _ip = '';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +66,77 @@ class _SettingsPageState extends State<SettingsPage> {
           )
         ],
       ),
+      body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Text(
+                          _connected
+                              ? 'Connected'
+                              : 'Establish connection to the system',
+                          style: TextStyle(
+                              color: _connected
+                                  ? ThemeColors.success
+                                  : Colors.white,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20))),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Input(
+                          controller: _ipController,
+                          label: 'IP',
+                          hint: '192.168.10.21',
+                          onChange: (value) {
+                            setState(() {
+                              _ip = value;
+                            });
+                          })),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Input(
+                          controller: _portController,
+                          label: 'Port',
+                          hint: '3000',
+                          onChange: (value) {
+                            setState(() {
+                              _port = value;
+                            });
+                          })),
+                  Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Button(
+                          label: 'Connect',
+                          width: 140,
+                          height: 48,
+                          loading: _loading,
+                          icon: Icon(Icons.connected_tv_rounded,
+                              color: ThemeColors.backgroundColor),
+                          onPressed: () async {
+                            // TODO: Implement connection logic.
+
+                            setState(() {
+                              _loading = true;
+                            });
+
+                            await Future.delayed(const Duration(seconds: 2));
+
+                            setState(() {
+                              _connected = !_connected;
+                              _loading = false;
+                            });
+                          })),
+                ],
+              ))
+            ],
+          )),
     );
   }
 }
