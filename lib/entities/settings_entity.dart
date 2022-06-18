8edc848561683b70b44c97298f64de419c966125
Michell Algarra <michell.algarra@gmail.com>
2022-06-18 18:54:01 -0300

    feat(entities/settings_entity.dart): implemented the settings entity with all of its properties and methods

---
+++ lib/entities/settings_entity.dart
@@ -0,0 +1,37 @@
+/// Class that represents the settings entity, containing its
+/// properties and methods.
+class SettingsEntity {
+  /// Property that defines the Liquid Galaxy master username.
+  /// Defaults to 'lg'.
+  String username;
+
+  /// Property that defines the Liquid Galaxy master password.
+  String password;
+
+  /// Property that defines the Liquid Galaxy master IP.
+  String ip;
+
+  /// Property that defines the Liquid Galaxy master SSH port.
+  /// Defaults to 22.
+  int port;
+
+  SettingsEntity(
+      {this.username = 'lg', this.password = '', this.ip = '', this.port = 22});
+
+  /// Turns a `Map` into a `SettingsEntity`.
+  factory SettingsEntity.fromMap(Map<String, dynamic> map) {
+    return SettingsEntity(
+        username: map['username'],
+        password: map['password'],
+        ip: map['ip'],
+        port: map['port']);
+  }
+
+  /// Return a `Map` from the current `SettingsEntity`.
+  Map<String, dynamic> toMap() => {
+        'username': username,
+        'password': password,
+        'ip': ip,
+        'port': port,
+      };
+}
