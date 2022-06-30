81f44fd72af564de84cb8ac91423aa5d1c0cec59
Michell Algarra <michell.algarra@gmail.com>
2022-06-30 04:08:26 -0300

    feat(data_list.dart): improved the list view using list builder

--- lib/views/data_list.dart
+++ lib/views/data_list.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:satnogs_visualization_tool/widgets/ground_station_card.dart';
+import 'package:satnogs_visualization_tool/widgets/satellite_card.dart';
 
 class DataList extends StatelessWidget {
-  const DataList({Key? key, required this.items}) : super(key: key);
+  const DataList({Key? key, required this.items, required this.render})
+      : super(key: key);
 
-  final List<Widget> items;
+  final List<dynamic> items;
+  final String render;
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return ListView.builder(
       padding: const EdgeInsets.only(bottom: 48),
       physics:
           const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: items,
-      ),
+      shrinkWrap: true,
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 16),
+          child: render == 'satellite'
+              ? SatelliteCard(satellite: items[index])
+              : GroundStationCard(groundStation: items[index]),
+        );
+      },
     );
+    //   ],
+    // );
   }
 }
