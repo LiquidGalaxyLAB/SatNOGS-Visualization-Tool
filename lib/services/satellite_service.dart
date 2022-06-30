80336a4c1b7e14e9149f2bccbea587cfeeacc13e
Michell Algarra <michell.algarra@gmail.com>
2022-06-30 04:11:12 -0300

    feat(ground_station_service.dart & satellite_service.dart): implemented the local storage data saving and reading

--- lib/services/satellite_service.dart
+++ lib/services/satellite_service.dart
@@ -1,12 +1,18 @@
 import 'dart:convert';
 
+import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 import 'package:satnogs_visualization_tool/entities/satellite_entity.dart';
+import 'package:satnogs_visualization_tool/services/local_storage_service.dart';
 import 'package:satnogs_visualization_tool/utils/api.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:satnogs_visualization_tool/utils/storage_keys.dart';
 
 /// Service that deals with all satellite data management.
 class SatelliteService {
+  LocalStorageService get _localStorageService =>
+      GetIt.I<LocalStorageService>();
+
   /// Property that defines the satellite access endpoint.
   final _endpoint = 'satellites';
 
@@ -22,12 +28,25 @@ class SatelliteService {
 
   /// Gets multiple satellites from the database based on the given [params].
   Future<List<SatelliteEntity>> getMany(
-      {Map<String, String>? params, List<String>? join}) async {
+      {Map<String, String>? params,
+      List<String>? join,
+      bool synchronize = false}) async {
+    final hasLocal = _localStorageService.hasItem(StorageKeys.satellites);
+
+    if (!synchronize && hasLocal) {
+      List<dynamic> localSatellites =
+          json.decode(_localStorageService.getItem(StorageKeys.satellites));
+
+      return localSatellites.map((s) => SatelliteEntity.fromMap(s)).toList();
+    }
+
     String query = API.buildQuery(params);
 
     final response = await http.get(
         Uri.parse('${API.baseUrl}/$_endpoint/?format=json&$query'),
         headers: {'Authorization': 'Token ' + dotenv.env['API_KEY']!});
+
+    _localStorageService.setItem(StorageKeys.satellites, response.body);
 
     final res = json.decode(response.body) as List<dynamic>;
     return res.map((s) => SatelliteEntity.fromMap(s)).toList();
