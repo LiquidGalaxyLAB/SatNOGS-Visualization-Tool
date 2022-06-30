262f5b18cc4a031d370b33861d76fabcdf83e858
Michell Algarra <michell.algarra@gmail.com>
2022-06-29 21:01:05 -0300

    feat(storage_keys.dart): added the satellites and ground stations keys

--- lib/utils/storage_keys.dart
+++ lib/utils/storage_keys.dart
@@ -2,4 +2,10 @@
 class StorageKeys {
   /// SVT_SETTINGS
   static const settings = 'SVT__SETTINGS';
+
+  /// SVT__SATELLITES
+  static const satellites = 'SVT__SATELLITES';
+
+  /// SVT__GROUND_STATIONS
+  static const groundStations = 'SVT__GROUND_STATIONS';
 }
