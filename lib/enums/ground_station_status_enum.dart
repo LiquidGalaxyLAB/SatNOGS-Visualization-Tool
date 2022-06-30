2edf63ca27246862c1573318a8a5232381d07acb
Michell Algarra <michell.algarra@gmail.com>
2022-06-29 21:05:08 -0300

    feat(enums/*): added all application enums

---
+++ lib/enums/ground_station_status_enum.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+/// Enum that defines all the ground station possible status.
+enum GroundStationStatusEnum {
+  ONLINE,
+  TESTING,
+  OFFLINE,
+}
---
+++ lib/enums/transmitter_status_enum.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: constant_identifier_names
+
+/// Enum that defines all the transmitter status.
+enum TransmitterStatusEnum {
+  ACTIVE,
+  INACTIVE,
+  INVALID,
+}
