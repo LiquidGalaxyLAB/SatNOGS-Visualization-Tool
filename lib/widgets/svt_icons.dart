1b159369725c1dabadf647db477446ede7fa6e0d
Michell Algarra <michell.algarra@gmail.com>
2022-07-04 23:52:02 -0300

    feat(assets/fonts/SVT.ttf & widgets/svt_icons.dart): added custom icons

---
+++ lib/widgets/svt_icons.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/widgets.dart';
+
+class SVT {
+  SVT._();
+
+  static const _kFontFam = 'SVT';
+  static const String? _kFontPkg = null;
+
+  static const IconData satellite_dish =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
