768f29c10200b5e45682f0c3f98e17d205723c99
Michell Algarra <michell.algarra@gmail.com>
2022-08-11 12:22:44 -0300

    feat(fonts/SVT.ttf & svt_icons.dart): added some icons to be used into the about page

--- lib/widgets/svt_icons.dart
+++ lib/widgets/svt_icons.dart
@@ -10,4 +10,8 @@ class SVT {
 
   static const IconData satellite_dish =
       IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData github =
+      IconData(0xf09b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData linkedin_in =
+      IconData(0xf0e1, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
