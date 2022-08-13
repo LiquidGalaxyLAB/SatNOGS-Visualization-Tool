90fa3f3be8c9f571916ae6b799467d9b771133d4
Michell Algarra <michell.algarra@gmail.com>
2022-08-13 02:49:46 -0300

    feat(fonts/SVT.ttf & svt_icons.dart): added the social network app logos

--- lib/widgets/svt_icons.dart
+++ lib/widgets/svt_icons.dart
@@ -10,8 +10,14 @@ class SVT {
 
   static const IconData satellite_dish =
       IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData twitter =
+      IconData(0xf099, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData github =
       IconData(0xf09b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData linkedin_in =
       IconData(0xf0e1, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData instagram =
+      IconData(0xf16d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData google_play =
+      IconData(0xf3ab, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
